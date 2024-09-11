00bae537a30dc3e3503684d2c00a2fe07b1ba822
lfarssi <medlfarssi10@gmail.com>
2024-09-11 10:42:32 +0100

    fsdf

--- func/functions.go
+++ func/functions.go
@@ -1,6 +1,8 @@
 package functions
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -28,7 +30,7 @@ func TextFormated(s []string) string {
 	return strings.TrimSpace(res)
 }
 
-func Quote(s string) string {
+func HandleQuote(s string) string {
 	var result string
 	wordInside := ""
 	quoteOpen := false
@@ -65,13 +67,145 @@ func IsVowel(s string) bool {
 	}
 	return false
 }
-func HandleVowel(arr []string, i int) []string {
-	if strings.ToLower(arr[i]) == "a" && i+1 < len(arr) && IsVowel(arr[i+1]) {
-		arr[i] += "n"
-	} else if strings.ToLower(arr[i]) == "an" && i+1 < len(arr) && !IsVowel(arr[i+1]) {
-		arr[i] = arr[i][:len(arr[i])-1]
+func HandleVowel(s string) string {
+	arr := strings.Fields(s)
+	for i := 0 ; i < len(arr); i++{
+		if strings.ToLower(arr[i]) == "a" && i+1 < len(arr) && IsVowel(arr[i+1]) {
+			arr[i] += "n"
+		} else if strings.ToLower(arr[i]) == "an" && i+1 < len(arr) && !IsVowel(arr[i+1]) {
+			arr[i] = arr[i][:len(arr[i])-1]
+		}
 	}
-	return arr
+	str := ""
+	for _, ch := range arr{
+		str += ch + " "
+	}
+	return str
+}
+
+func HandleKeyword(s string) string{
+			t := ""
+			insideParenthese := false
+			for _, v := range s {
+				if v == '(' {
+					t += " " + string(v)
+					insideParenthese = true
+				} else if v == ')' {
+					t += string(v) + " "
+					insideParenthese = false
+
+				} else {
+					if insideParenthese {
+						if v == ',' {
+							t += string(v)
+						} else if v != ' ' {
+							t += string(v)
+						}
+					} else {
+						if v == ',' || v == '.' || v == ':' || v == '!' || v == '?' || v == ';' {
+							t += " " + string(v) + " "
+						} else {
+							t += string(v)
+						}
+					}
+				}
+
+			}
+
+			arr1 := strings.Fields(string(t))
+			res2 := ""
+			for _, item := range arr1 {
+				if strings.HasPrefix(item, "(") && strings.HasSuffix(item, ")") {
+					content := item[1 : len(item)-1]
+					if strings.Contains(content, ",") {
+						res2 += "(" + content + ") "
+					} else {
+						switch content {
+						case "cap":
+							res2 += "(cap,1) "
+						case "low":
+							res2 += "(low,1)"
+						case "up":
+							res2 += "(up,1)"
+						case "hex", "bin":
+							res2 += "(" + content + ") "
+						default:
+							res2 += "(" + content + ") "
+						}
+					}
+				} else {
+					res2 += item + " "
+				}
+
+			}
+			arr := strings.Fields(res2)
+			for i := 0; i < len(arr); i++ {
+				insideParenthese2 := false
+				if strings.HasPrefix(arr[i], "(") && strings.HasSuffix(arr[i], ")") {
+					insideParenthese2 = true
+				} else {
+					insideParenthese2 = false
+				}
+				var Akwas []string
+				var action string
+				var nb int
+				if insideParenthese2 {
+					arr[i] = strings.Trim(arr[i], "()")
+					Akwas = strings.Split(arr[i], ",")
+					arr[i] = ""
+					i--
+					action = Akwas[0]
+					if len(Akwas) == 2 {
+						nb, err := strconv.Atoi(Akwas[1])
+						if err != nil {
+							fmt.Println("msg err : not a number ", err)
+							continue
+						}
+					}
+				}
+				if action == "cap" || action == "low" || action == "up" {
+					for j := 1; j <= nb; j++ {
+						if i-j < 0 {
+							break
+						}
+						if !IsWord(arr[i-j]) {
+							continue
+						}
+						if action == "cap" {
+							arr[i-j] = Capitalize(arr[i-j])
+
+						} else if action == "low" {
+							arr[i-j] = ToLower(arr[i-j])
+
+						} else if action == "up" {
+							arr[i-j] = ToUpper(arr[i-j])
+
+						}
+					}
+
+				} else if action == "bin" {
+					integer, err := strconv.ParseInt(arr[i-1], 2, 64)
+					if err != nil {
+						i -= 2
+						continue
+					}
+					arr[i-1] = strconv.FormatInt(integer, 10)
+					arr[i] = ""
+					i--
+
+				} else if action == "hex" {
+					integer, err := strconv.ParseInt(arr[i-1], 16, 64)
+					if err != nil {
+						fmt.Println("you can't convert")
+						continue
+					}
+					arr[i-1] = strconv.FormatInt(integer, 10)
+					arr[i] = ""
+					i--
+
+				}
+			}
+	return s
 }
 
 func IsAlphabet(s byte) bool {
